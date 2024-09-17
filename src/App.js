037f247d52ca0e285b363079a2c6926dff075de2
nipunisf99 <fernandopns.19@uom.lk>
2024-09-17 23:45:00 +0530

    hashcode changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Routes, Route, Outlet  } from 'react-router-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 
 import Home from "./Components/Home";
